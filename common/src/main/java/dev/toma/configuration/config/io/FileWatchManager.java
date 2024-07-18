5cd6ab9418d0fc28e1ab47ccf3745c221e960c53
Toma <novotny.tom96@gmail.com>
2024-07-19 00:42:50 +0200

    Changed auto-sync thread to daemon thread. Resolves #17

--- common/src/main/java/dev/toma/configuration/config/io/FileWatchManager.java
+++ common/src/main/java/dev/toma/configuration/config/io/FileWatchManager.java
@@ -34,6 +34,7 @@ public final class FileWatchManager {
             this.executorService = Executors.newSingleThreadScheduledExecutor(r -> {
                 Thread t = new Thread(r);
                 t.setName("Auto-Sync thread");
+                t.setDaemon(true);
                 return t;
             });
         }
