e6dfed6e9feec4ed945b5e924a96e377d41c255d
Toma <novotny.tom96@gmail.com>
2024-10-20 19:00:06 +0200

    Fixed Fabric workspace

--- fabric/src/main/java/dev/toma/configuration/network/FabricNetworkManager.java
+++ fabric/src/main/java/dev/toma/configuration/network/FabricNetworkManager.java
@@ -8,6 +8,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.client.Minecraft;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
@@ -39,8 +40,13 @@ public class FabricNetworkManager implements NetworkManager {
     private <T extends NetworkMessage> void registerMessageS2C(ResourceLocation pid, Function<FriendlyByteBuf, T> constructor) {
         ClientPlayNetworking.registerGlobalReceiver(pid, (client, handler, buf, responseSender) -> {
             T message = constructor.apply(buf);
-            client.execute(() -> message.handle(client.player));
+            client.execute(() -> handlePayload(message, client));
         });
+    }
+
+    @Environment(EnvType.CLIENT)
+    private static <T extends NetworkMessage> void handlePayload(T message, Minecraft client) {
+        message.handle(client.player);
     }
 
     private static <T extends NetworkMessage> void dispatch(T message, BiConsumer<ResourceLocation, FriendlyByteBuf> dispatcher) {
